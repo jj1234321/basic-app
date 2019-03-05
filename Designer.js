db3446cecb5e55231e337fc24cea657ed06efad1
jj1234321 <46678645+jj1234321@users.noreply.github.com>
2019-03-05 17:46:12 -0500

    Add files via upload
    Made it so that the designer outputs code.

--- Designer.js
+++ Designer.js
@@ -5,27 +5,46 @@ import './Designer.css';
 import HeadshotResult from './HeadshotResult';
 
 class Designer extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            preppedCode : "test",
+            showCode: false
+        }
+    }
     //Should have item in props.
 
     handleExample = (event) => {
         event.preventDefault();
         let filterNamesAndValues = [];
         let contentToSearch = false;
+        let newPreppedCode = "{";
         let example = {};
         for (var exampleFieldToRender of event.currentTarget) {
             if (exampleFieldToRender.type == "text") {
                 if(exampleFieldToRender.value){
-                    example[exampleFieldToRender.name.toLowerCase()] = exampleFieldToRender.value;            
+                    const preppedValue = exampleFieldToRender.name.toLowerCase();
+                    example[preppedValue] = exampleFieldToRender.value;
+                    newPreppedCode = newPreppedCode + `\"${preppedValue}\" : \"${exampleFieldToRender.value}\",\n`;
                 } else{
+                    const preppedValue = exampleFieldToRender.name.toLowerCase();
                     example[exampleFieldToRender.name.toLowerCase()] = 'N/A';            
-                
+                    newPreppedCode = newPreppedCode + `\"${preppedValue}\" : \"N\\A\",\n`;
                 }
             }
         }
+        newPreppedCode = newPreppedCode.substring(0, newPreppedCode.length - 2);
+        newPreppedCode = newPreppedCode + `\n}`
+        this.setState({preppedCode: newPreppedCode});
 
         ReactDOM.render(<HeadshotResult item={example} />, document.getElementById('example'))
 
         return false;
+    }
+
+    selectCode = () => {
+        document.getElementById("code-area").focus();
+        document.getElementById("code-area").select();
     }
 
     render() {
@@ -43,6 +62,9 @@ class Designer extends Component {
                         {objectsForInformation}
                         <input className="example-button" type="submit" value="Submit" />
                     </form>
+                    <button id="display-code" onClick={() => this.setState({showCode : true})} className="basic-button" style={{'display' : this.state.preppedCode ? 'flex' : 'none'}}>Generate Code</button>
+                    <textarea readOnly id="code-area" style={{'display' : this.state.showCode ? 'flex' : 'none'}} className="prepped-code" value={this.state.preppedCode}></textarea>
+                    <button id="select-text" style={{'display' : this.state.showCode ? 'flex' : 'none'}} className="basic-button" onClick={() => this.selectCode()}>Select Code</button>
                 </span>
             </span>
 
